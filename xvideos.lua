1da3fb3236734907dbf2af7e2d726d6658b03bf4
Comsos <150440311+sndqk@users.noreply.github.com>
2024-02-18 09:37:58 +0700

    Update xvideos.lua

--- xvideos.lua
+++ xvideos.lua
@@ -1,5 +1,5 @@
 local ScrW = ScrW()
-X = ScrW * 1.02
+local X = ScrW * 1.02
 surface.CreateFont('XVIDEOS FONT', { font = 'Verdana', size = 64, antialias = false, shadow = true } )
 local colorWatemark = Color( 232, 232, 232, 235)
 hook.Add('HUDPaint', 'WWW.XVIDEOS.COM', function()
